2d1e24228564070471c796e2fc15a3a4fe66efaf
jparcar <115104686+jparcar@users.noreply.github.com>
2022-12-21 20:49:17 +0100

    Add basic controller

---
+++ Assets/Script/Constants/BallState.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Assets.Script.Constants
+{
+    [CreateAssetMenu(fileName = "BallState", menuName = "BallState", order = 0)]
+    public class BallState : ScriptableObject
+    {
+
+    }
+}
+
+public enum BallEstate
+{
+
+}
---
+++ Assets/Script/HUDController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private bool butted = false;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void clickInBall()
+    {
+        Vector3 posicionRaton = Input.mousePosition;
+        Vector2 posicionMundo = (Vector2)Camera.main.ScreenToWorldPoint(posicionRaton);
+        transform.position = posicionMundo;
+    }
+}
